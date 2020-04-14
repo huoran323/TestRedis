e61b4f90453717d3ab0507e0a4bcee70d2e64cee
xiaoran <303176152@qq.com>
2020-04-14 09:32:55 +0800

    submit

--- src/com/hr/test/Test.java
+++ src/com/hr/test/Test.java
@@ -1,5 +1,17 @@
 package com.hr.test;
 
+import redis.clients.jedis.Jedis;
+
 public class Test {
 
+	public static void main(String[] args) {
+		
+		Jedis jedis = new Jedis("192.168.95.128", 6379);
+		String result = jedis.ping();
+		System.out.println(result);
+		jedis.set("a", "张三");
+		String a = jedis.get("a");
+		System.out.println(a);
+		jedis.close();
+	}
 }
